e3992e453170e6c5d7a5b4a94a6e7b5e11ab0dbe
fyyyyyyy <877385364@qq.com>
2025-01-28 14:26:14 +0800

    feat(layout/header): 一级导航对接接口渲染

--- src/views/Layout/components/LayoutHeader.vue
+++ src/views/Layout/components/LayoutHeader.vue
@@ -1,9 +1,55 @@
 <script setup>
+import { getCategoryAPI } from '@/apis/layout';
+import { ref, onMounted } from 'vue';
+import { RouterLink } from 'vue-router';
 
+const categoryList = ref([]);
+const getCategory = async () => {
+  const res = await getCategoryAPI();
+  categoryList.value = res.result;
+}
+onMounted(() => {
+  getCategory()
+})
 </script>
 
 <template>
+  <div class="container">
+    <img src="@/assets/images/logo.png" alt="">
+    <ul>
+      <li v-for="category in categoryList" :Key="category.id">
+        <RouterLink to="/">{{ category.name }}</RouterLink>
+      </li>
+    </ul>
+    <el-input v-model="input2" style="width: 240px" placeholder="select" :suffix-icon="Search" />
+
+  </div>
+
+
 
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  ul {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    margin-right: 28vw;
+
+    li {
+      font-size: 14px;
+      margin: 15px;
+    }
+
+  }
+
+  img {
+    width: 10vw;
+  }
+}
+</style>
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,12 +1,12 @@
 <script setup>
 import LayoutNav from './components/LayoutNav.vue';
 import LayoutFooter from './components/LayoutFooter.vue';
-import LayoutFixed from './components/LayoutFixed.vue';
+import LayoutHeader from './components/LayoutHeader.vue';
 </script>
 
 <template>
   <LayoutNav></LayoutNav>
-  <LayoutFixed></LayoutFixed>
+  <LayoutHeader></LayoutHeader>
   <router-view></router-view>
   <LayoutFooter></LayoutFooter>
 </template>
