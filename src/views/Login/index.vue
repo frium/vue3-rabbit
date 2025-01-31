bf9205acbae14c9505e6f7f8bc1cb84c270d9844
fyyyyyyy <877385364@qq.com>
2025-01-31 23:40:27 +0800

    feat(login): 添加登录注册页面

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,9 +1,268 @@
-<template>
-  <span>登录</span>
-</template>
-
 <script setup>
+import { ref } from 'vue';
+const user = {
+  username: '',
+  password: ''
+}
 
+const login = ref({ user });
+const register = ref({ user });
+const isLogin = ref(true);//switch控制状态
+const changState = () => {
+  isLogin.value = !isLogin.value
+
+}
 </script>
 
-<style></style>
+<template>
+  <div class="container">
+    <div class="shell">
+      <div class="register box  " :style="{
+        left: isLogin ? '400px' : '0px',
+        opacity: isLogin ? 0 : 1
+      }">
+        <form action="" class="register-form">
+          <span>创建账号</span>
+          <input type="text" placeholder="username" v-model="register.name">
+          <input type="password" placeholder="password" v-model="register.password">
+          <button>SIGN UP</button>
+        </form>
+      </div>
+      <div class="login box" :style="{
+        left: isLogin ? '400px' : '0px',
+        opacity: isLogin ? 1 : 0
+      }">
+        <form action="" class="login-form">
+          <span>登录账号</span>
+          <input type="text" placeholder="username" v-model="login.name">
+          <input type="password" placeholder="password" v-model="login.password">
+          <a href="/">忘记密码?</a>
+          <button>SIGN IN</button>
+        </form>
+      </div>
+      <div class="switch" :style="{ left: isLogin ? '0px' : '600px' }">
+        <div class="circle"></div>
+        <div class="switch-to-register switch-container " :style="{ opacity: isLogin ? 0 : 1 }">
+          <span>Welcome Login!</span>
+          <p>已经有账号了嘛,去登录探索吧!</p>
+          <button @click="changState">SIGN IN</button>
+        </div>
+        <div class="switch-to-login switch-container" :style="{ opacity: isLogin ? 1 : 0 }">
+          <span>Welcome Register!</span>
+          <p>还没有账号? 速速注册吧~</p>
+          <button @click="changState">SIGN UP</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<style scoped lang="scss">
+@keyframes moveForm {
+  from {
+    left: 400px;
+  }
+
+  to {
+    left: 0;
+  }
+
+}
+
+@keyframes moveSwitch {
+  from {
+    left: 0;
+  }
+
+  to {
+    left: 600px;
+  }
+}
+
+
+.container {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  a {
+    font-size: 16px;
+    margin-top: 3px;
+  }
+
+  button {
+    width: 180px;
+    height: 50px;
+    border-radius: 25px;
+    margin-top: 50px;
+    font-weight: 700;
+    font-size: 14px;
+    letter-spacing: 1.15px;
+    background-color: #4B70E2;
+    color: #f9f9f9;
+    box-shadow: 8px 8px 16px #d1d9e6, -8px -8px 16px #f9f9f9;
+    border: none;
+    outline: none;
+    pointer-events: auto;
+
+    &:hover {
+      cursor: pointer;
+    }
+  }
+
+  .shell {
+    position: relative;
+    width: 1000px;
+    height: 600px;
+    padding: 25px;
+    background-color: #ecf0f3;
+    box-shadow: 10px 10px 10px #d1d9e6, -10px -10px 10px #f9f9f9;
+    border-radius: 12px;
+    overflow: hidden;
+
+    .box {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: absolute;
+      top: 0;
+      width: 600px;
+      height: 100%;
+      padding: 25px;
+      background-color: #ecf0f3;
+      transition:
+        left 1.25s cubic-bezier(0.68, -0.55, 0.265, 1.55),
+        opacity 0.01s ease 0.58s;
+
+      form {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        width: 100%;
+        height: 100%;
+        animation: moveForm 1.25s;
+
+
+        span {
+          font-size: 34px;
+          font-weight: 700;
+          line-height: 3;
+          color: #181818;
+          letter-spacing: 10px;
+        }
+
+        input {
+          width: 350px;
+          height: 40px;
+          margin: 4px 0;
+          padding-left: 25px;
+          font-size: 13px;
+          letter-spacing: 0.15px;
+          border: none;
+          outline: none;
+          background-color: #ecf0f3;
+          transition: 0.25s ease;
+          border-radius: 8px;
+          box-shadow: inset 2px 2px 4px #d1d9e6, inset -2px -2px 4px #f9f9f9;
+
+          &:focus {
+            box-shadow: inset 4px 4px 4px #d1d9e6, inset -4px -4px 4px #f9f9f9;
+
+          }
+        }
+      }
+
+
+    }
+
+    .login {
+      left: 400px;
+      opacity: 1;
+    }
+
+    .register {
+      left: 400px;
+      opacity: 0;
+    }
+
+
+
+    .switch {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: absolute;
+      top: 0;
+      left: 0;
+      height: 100%;
+      width: 400px;
+      padding: 50px;
+      z-index: 200;
+      transition: left 1.25s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+      background-color: #ecf0f3;
+      overflow: hidden;
+      box-shadow: 4px 4px 10px #d1d9e6, -4px -4px 10px #d1d9e6;
+      animation: moveSwtich 1.25s;
+
+      .circle {
+        position: absolute;
+        width: 500px;
+        height: 500px;
+        border-radius: 50%;
+        background-color: #ecf0f3;
+        box-shadow: inset 8px 8px 12px #e1c7da, inset -8px -8px 12px #fff;
+        transition: transform 1s ease;
+        transform: rotate(0deg);
+
+        &:hover {
+          transform: rotate(180deg);
+        }
+
+
+
+      }
+
+      .switch-container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        position: absolute;
+        width: 400px;
+        padding: 50px 55px;
+        transition: 1.2s;
+        pointer-events: none;
+      }
+
+      .switch-to-register {
+        z-index: 99;
+
+      }
+
+      .switch-to-login {
+        z-index: 99;
+      }
+
+      span {
+        font-size: 30px;
+        margin-bottom: 10px;
+        letter-spacing: 0.25px;
+        color: #181818;
+        text-align: center;
+        line-height: 1.6;
+      }
+
+      p {
+        font-size: 16px;
+      }
+    }
+
+  }
+
+
+}
+</style>
